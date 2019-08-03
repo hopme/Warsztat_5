130772db9d6a1677203434e7018b52cf467187cc
AdamSzambur <adamszamburski@gmail.com>
2019-08-03 14:29:21 +0200

    Łukasz

--- src/main/java/pl/coderslab/controllers/BookController.java
+++ src/main/java/pl/coderslab/controllers/BookController.java
@@ -2,6 +2,7 @@ package pl.coderslab.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.coderslab.beans.Book;
 
 @RestController
 @RequestMapping("/books")
@@ -10,4 +11,11 @@ public class BookController {
     public String hello(){
         return "{hello: World}";
     }
+
+    @RequestMapping("/helloBook")
+    public Book helloBook(){
+        return new Book(1L,"9788324631766","Thinking in Java",
+                "Bruce Eckel","Helion","programming");
+    }
+
 }
