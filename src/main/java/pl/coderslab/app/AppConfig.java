a4e3b4f525522694574078d8f44aa2300e5e90c6
AdamSzambur <adamszamburski@gmail.com>
2019-08-03 14:55:48 +0200

    Tomasz

--- src/main/java/pl/coderslab/app/AppConfig.java
+++ src/main/java/pl/coderslab/app/AppConfig.java
@@ -2,6 +2,7 @@ package pl.coderslab.app;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -10,5 +11,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @ComponentScan(basePackages = "pl.coderslab")
 public class AppConfig extends WebMvcConfigurerAdapter {
 
-
+    @Override
+    public void configureDefaultServletHandling(
+            DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 }
--- src/main/java/pl/coderslab/controllers/BookController.java
+++ src/main/java/pl/coderslab/controllers/BookController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.coderslab.beans.Book;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/books")
 public class BookController {
@@ -12,10 +15,12 @@ public class BookController {
         return "{hello: World}";
     }
 
-    @RequestMapping("/helloBook")
-    public Book helloBook(){
-        return new Book(1L,"9788324631766","Thinking in Java",
-                "Bruce Eckel","Helion","programming");
+    @RequestMapping("/")
+    public List<Book> helloBook(){
+        List<Book> books = new ArrayList<>();
+        books.add(new Book(1L,"9788324631766","Thinking in Java",
+                "Bruce Eckel","Helion","programming"));
+        return books;
     }
 
 }
